7c02a3914d8fc56265b2dfda9abca98d751a61b8
Sheng Yue <63217865+shaunyue@users.noreply.github.com>
2022-11-07 01:52:36 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,30 +3,24 @@
 import os
 from setuptools import setup
 
-def get_version() -> str:
-    # https://packaging.python.org/guides/single-sourcing-package-version/
-    init = open(os.path.join("offlinerl", "__init__.py"), "r").read().split()
-    return init[init.index("__version__") + 2][1:-1]
-
 setup(
-    name='offlinerl',
-    description="A Library for Offline RL(Batch RL)",
-    url="https://agit.ai/Polixir/OfflineRL",
-    version=get_version(),
-    author="SongyiGao",
-    author_email="songyigao@gmail.com",
+    name='clare',
     python_requires=">=3.7",
     install_requires=[
         "aim==2.0.27",
         "fire",
-        "loguru",
-        "gym",
-        "sklearn",
+        "loguru==0.6.0",
+        "gym==0.23.1",
+        "sklearn==0.0",
         "gtimer",
-        "numpy",
-        "tianshou",
-        "tqdm"
-        # "mujoco-py==2.0.2.13",
+        "numpy==1.21.6",
+        "tianshou==0.4.8",
+        "tqdm",
+        "mujoco-py<2.2,>=2.1",
+        "scikit-learn==1.0.2",
+        "scipy==1.7.3",
+        "ray==1.12.0",
+        "patchelf"
+        #"mujoco-py==2.0.2.13",
     ],
-    
 )
